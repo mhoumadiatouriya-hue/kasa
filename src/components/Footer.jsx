c9dd58534b55a32d65353718a28a207f2ea45157
Atourya Mhoumadi <mhoumadi.atouriya@gmail.com>
2026-03-20 14:58:17 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import "./footer.css";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="footer">
+      <div className="footer-content">
+        <h2 className="footer-logo">Kasa</h2>
+        <p>© 2020 Kasa. All rights reserved</p>
+      </div>
     </footer>
   );
 }
