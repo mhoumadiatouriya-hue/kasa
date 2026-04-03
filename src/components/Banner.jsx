fb8fceb7720b2411802e6de396899cb5f4d7f58b
Atourya Mhoumadi <mhoumadi.atouriya@gmail.com>
2026-04-03 10:30:30 +0200

    correction Banner (CSS déplacé + affichage image fond)

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,23 +1,9 @@
-.banner {
-  position: relative;
-  height: 223px;
-  border-radius: 25px;
-  overflow: hidden;
-  background-image:
-    linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-    url("/images/banner-home.png");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+import "./banner.css";
 
-.banner h1 {
-  margin: 0;
-  color: #ffffff;
-  font-size: 48px;
-  font-weight: 700;
-  text-align: center;
+export default function Banner() {
+  return (
+    <section className="banner">
+      <h1>Chez vous, partout et ailleurs</h1>
+    </section>
+  );
 }
